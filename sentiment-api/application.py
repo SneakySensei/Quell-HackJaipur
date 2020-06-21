0728dae270dd5e4094ae70002b96da18a77d7697
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-06-21 09:38:47 +0530

    returns jsonified response

--- sentiment-api/application.py
+++ sentiment-api/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, request
+from flask import Flask, abort, request, jsonify
 from flair.models import TextClassifier
 from flair.data import Sentence
 from flask import render_template
@@ -16,6 +16,6 @@ def sentimentAnalysis():
 		label = sentence.labels[0]
 		labscore = (label.score)*100
 		response = {'result': label.value, 'score': "%.2f" % labscore}
-		return render_template('index.html', query=inputQuery, output=response)
+		return jsonify(response)
 	else:
 		return render_template('index.html') 
