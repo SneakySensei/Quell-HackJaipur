f759ba05bb82a212d084004a2b2a645f61515dc4
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-06-21 11:16:06 +0530

    removed the label obj

--- sentiment-api/application.py
+++ sentiment-api/application.py
@@ -15,7 +15,6 @@ def sentimentAnalysis():
 	sentence = Sentence(final)
 	classifier.predict(sentence)
 	print('Sentiment: ', sentence.labels)
-	label = sentence.labels[0]
 	labscore = (label.score)*100
-	response = {'result': label.value, 'score': "%.2f" % labscore, 'sen': final}
+	response = {'score': "%.2f" % labscore}
 	return jsonify(response)
