706d9f18c94ffb30929dc9f8dc36e1f1e4d9d958
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-06-21 10:48:51 +0530

    performing sentiment an on received json

--- sentiment-api/application.py
+++ sentiment-api/application.py
@@ -5,23 +5,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST', 'GET'])
-def sentimentAnalysis():
-	if request.method == 'POST':
-		classifier = TextClassifier.load('en-sentiment')
-		inputQuery = request.form['query']
-		sentence = Sentence(inputQuery)
-		classifier.predict(sentence)
-		print('Sentiment: ', sentence.labels)
-		label = sentence.labels[0]
-		labscore = (label.score)*100
-		response = {'result': label.value, 'score': "%.2f" % labscore}
-		return jsonify(response)
-	else:
-		return render_template('index.html') 
 
-@app.route("/json", methods=['POST'])
-def json():
-	req_data = request.get_json(force=True)
-	a = req_data['examples'][2]
-	return a
+@app.route("/json", methods=['POST', 'GET'])
+def sentimentAnalysis():
+
+  if request.method=='POST':
+    req_data = request.get_json(force=True)
+    message = req_data['messages'][0]
+    classifier = TextClassifier.load('en-sentiment')
+    inputQuery = message
+    sentence = Sentence(inputQuery)
+    classifier.predict(sentence)
+    print('Sentiment: ', sentence.labels)
+    label = sentence.labels[0]
+    labscore = (label.score)*100
+    response = {'result': label.value, 'score': "%.2f" % labscore}
+    return jsonify(response)
+  else:
+    return render_template('index.html')
