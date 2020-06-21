f6d4840b104540f76a167b74041e3a6b53bf5a3b
Sundaram Gupta <sundaramgupta18@gmail.com>
2020-06-21 11:23:08 +0530

    changed output format

--- sentiment-api/application.py
+++ sentiment-api/application.py
@@ -15,6 +15,7 @@ def sentimentAnalysis():
 	sentence = Sentence(final)
 	classifier.predict(sentence)
 	print('Sentiment: ', sentence.labels)
+	label = sentence.labels[0]
 	labscore = (label.score)*100
-	response = {'score': "%.2f" % labscore}
+	response = {'sentiment': label.value, 'score': "%.2f" % labscore}
 	return jsonify(response)
