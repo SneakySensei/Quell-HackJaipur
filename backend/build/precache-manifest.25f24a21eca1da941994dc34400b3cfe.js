ebe4a43d50ab7240e8e0147cd90849e2d46e8d69
Snehil <snehilwebber@gmail.com>
2020-06-25 05:35:29 +0530

    added build

---
+++ backend/build/precache-manifest.25f24a21eca1da941994dc34400b3cfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f3747b6824c46f213cf2c67b248ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616ec815cf380399d7a8",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d7f333237358939778f7",
+    "url": "/static/css/main.5b84f823.chunk.css"
+  },
+  {
+    "revision": "616ec815cf380399d7a8",
+    "url": "/static/js/2.6561234d.chunk.js"
+  },
+  {
+    "revision": "6312c3b121251e9911f957bde7621230",
+    "url": "/static/js/2.6561234d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f333237358939778f7",
+    "url": "/static/js/main.b677f6e1.chunk.js"
+  },
+  {
+    "revision": "a592a4c727cb9346c70f",
+    "url": "/static/js/runtime-main.4aba4fbd.js"
+  },
+  {
+    "revision": "85cddc584ff9fa710e75e852a5cd14e6",
+    "url": "/static/media/user.85cddc58.svg"
+  }
+]);
